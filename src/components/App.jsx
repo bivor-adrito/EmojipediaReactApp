cfc480b6ea1bbb56aadd169fab3f599f9f555fa5
bivor-adrito <62353526+bivor-adrito@users.noreply.github.com>
2020-06-25 22:03:26 +0600

    complete non dynamic card

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import Card from "./Card";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Header />
 
       <dl className="dictionary">
+        <Card />
         <div className="term">
           <dt>
             <span className="emoji" role="img" aria-label="Tense Biceps">
